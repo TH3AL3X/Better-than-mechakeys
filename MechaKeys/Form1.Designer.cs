23893929af113b988b687f12a3f91cc27aa7d4c5
TH3AL3X <themachinehack@gmail.com>
2020-11-18 22:04:44 +0100

    Updated

---
+++ MechaKeys/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace keysimulator
+{
+    partial class MechaKeys
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MechaKeys));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.keysimulatornotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(21, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\Users\\funes\\Desktop\\MechaKeys\\MechaKeys\\bin\\Release\\audios\\keyboard3.mp3";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 230);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Ready";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Default keys";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Alt key";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(21, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(430, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "C:\\Users\\funes\\Desktop\\MechaKeys\\MechaKeys\\bin\\Release\\audios\\keyboard3.mp3";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Shift key";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(21, 123);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(430, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "C:\\Users\\funes\\Desktop\\MechaKeys\\MechaKeys\\bin\\Release\\audios\\keyboard3.mp3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Enter key";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Location = new System.Drawing.Point(21, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(430, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "C:\\Users\\funes\\Desktop\\MechaKeys\\MechaKeys\\bin\\Release\\audios\\keyboard3.mp3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Delete key";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Location = new System.Drawing.Point(21, 204);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(430, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "C:\\Users\\funes\\Desktop\\MechaKeys\\MechaKeys\\bin\\Release\\audios\\keyboard3.mp3";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox6.Location = new System.Drawing.Point(344, 228);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(35, 20);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "100";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(379, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "% volume";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(457, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "File mp3";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(457, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "File mp3";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(457, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "File mp3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(457, 161);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "File mp3";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(457, 202);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "File mp3";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(182, 227);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Save config";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(84, 231);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 17);
+            this.checkBox2.TabIndex = 19;
+            this.checkBox2.Text = "Start with WIN";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // keysimulatornotify
+            // 
+            this.keysimulatornotify.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.keysimulatornotify.BalloonTipText = "Minimized on system tray";
+            this.keysimulatornotify.BalloonTipTitle = "Clap clap clap";
+            this.keysimulatornotify.Icon = ((System.Drawing.Icon)(resources.GetObject("keysimulatornotify.Icon")));
+            this.keysimulatornotify.Text = "Key Simulator";
+            this.keysimulatornotify.Visible = true;
+            this.keysimulatornotify.DoubleClick += new System.EventHandler(this.keysimulatornotify_DoubleClick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(263, 227);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Reset config";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // MechaKeys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 270);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MechaKeys";
+            this.Text = "Key Simulator by Terror";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MechaKeys_FormClosed);
+            this.Load += new System.EventHandler(this.MechaKeys_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.MechaKeys_MouseDoubleClick);
+            this.Resize += new System.EventHandler(this.MechaKeys_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NotifyIcon keysimulatornotify;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ MechaKeys/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using NAudio.Wave;
+using Microsoft.Win32;
+
+namespace keysimulator
+{
+    public partial class MechaKeys : Form
+    {
+        //Seriously someone is selling a program like this, pathetic....
+        private IKeyboardMouseEvents m_GlobalHook;
+        static bool niggadontcrash;
+        public MechaKeys()
+        {
+            InitializeComponent();
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyDown += KeyDown;
+            m_GlobalHook.KeyPress += KeyPress;
+            m_GlobalHook.KeyUp += KeyUp;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+        }
+
+        public void Wait(int ms)
+        {
+            DateTime start = DateTime.Now;
+            while ((DateTime.Now - start).TotalMilliseconds < ms)
+                Application.DoEvents();
+        }
+
+        public void playSound(string textbox)
+        {
+            Wait(2);
+            IWavePlayer waveOutDevice = new WaveOut();
+            AudioFileReader audioFileReader = new AudioFileReader($"{textbox}");
+            //audioFileReader.Volume = int.Parse(textBox6.Text);
+            waveOutDevice.Init(audioFileReader);
+
+            float userVal;
+            if (float.TryParse(textBox6.Text, out userVal))
+            {
+            }
+
+            float volumenfix = 0.00f + userVal;
+            audioFileReader.Volume = volumenfix;
+            waveOutDevice.Play();
+        }
+
+        private void KeyPress(object sender, KeyPressEventArgs e)
+        {
+            niggadontcrash = true;
+        }
+
+        private void KeyUp(object sender, KeyEventArgs e)
+        {
+            niggadontcrash = false;
+        }
+        private void KeyDown(object sender, KeyEventArgs e)
+        {
+            if (checkBox1.Checked && niggadontcrash == false)
+            {
+                try
+                {
+                    if (e.KeyCode == Keys.Alt)
+                    {
+                        playSound(textBox2.Text);
+                    }
+                    else if (e.KeyCode == Keys.Shift)
+                    {
+                        playSound(textBox3.Text);
+                    }
+                    else if (e.KeyCode == Keys.Enter)
+                    {
+                        playSound(textBox4.Text);
+                    }
+                    else if (e.KeyCode == Keys.Delete)
+                    {
+                        playSound(textBox5.Text);
+                    }
+                    else
+                    {
+                        playSound(textBox1.Text);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Uuuups error getting the dir");
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox6.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                textBox6.Text = textBox6.Text.Remove(textBox6.Text.Length - 1);
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = textBox1.Text;
+            openFileDialog1.Filter = "mp3 (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                textBox1.Text = fileName;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = textBox2.Text;
+            openFileDialog1.Filter = "mp3 (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                textBox2.Text = fileName;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = textBox3.Text;
+            openFileDialog1.Filter = "mp3 (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                textBox3.Text = fileName;
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = textBox4.Text;
+            openFileDialog1.Filter = "mp3 (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                textBox4.Text = fileName;
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = textBox5.Text;
+            openFileDialog1.Filter = "mp3 (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                textBox5.Text = fileName;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MechaKeys_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            m_GlobalHook.KeyDown -= KeyDown;
+            m_GlobalHook.KeyPress -= KeyPress;
+            m_GlobalHook.KeyUp -= KeyUp;
+            m_GlobalHook.Dispose();
+
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (checkBox2.Checked)
+                rk.SetValue("keysimulator", Application.ExecutablePath);
+            else
+                rk.DeleteValue("keysimulator", false);
+
+            Properties.Settings.Default.checkbox = checkBox1.Checked;
+            Properties.Settings.Default.checkbox2 = checkBox2.Checked;
+            Properties.Settings.Default.textbox1 = textBox1.Text;
+            Properties.Settings.Default.textbox2 = textBox2.Text;
+            Properties.Settings.Default.textbox3 = textBox3.Text;
+            Properties.Settings.Default.textbox4 = textBox4.Text;
+            Properties.Settings.Default.textbox5 = textBox5.Text;
+            Properties.Settings.Default.textbox6 = textBox6.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void MechaKeys_Load(object sender, EventArgs e)
+        {
+            checkBox1.Checked = Properties.Settings.Default.checkbox;
+            checkBox2.Checked = Properties.Settings.Default.checkbox2;
+            textBox1.Text = Properties.Settings.Default.textbox1;
+            textBox2.Text = Properties.Settings.Default.textbox2;
+            textBox3.Text = Properties.Settings.Default.textbox3;
+            textBox4.Text = Properties.Settings.Default.textbox4;
+            textBox5.Text = Properties.Settings.Default.textbox5;
+            textBox6.Text = Properties.Settings.Default.textbox6;
+            Properties.Settings.Default.Reset();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MechaKeys_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                keysimulatornotify.Visible = true;
+                keysimulatornotify.ShowBalloonTip(1000);
+            }
+        }
+
+        private void MechaKeys_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            keysimulatornotify.Visible = false;
+        }
+
+        private void keysimulatornotify_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            keysimulatornotify.Visible = false;
+        }
+
+        private void thirteenTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
